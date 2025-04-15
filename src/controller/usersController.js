b1ac8f5d5725e4f662ec964fc5ca04f0295bcaf8
ivanribeirodf <ivandf@gmail.com>
2025-04-15 12:22:00 -0300

    System structure - Backend nodejs

---
+++ src/controller/usersController.js
@@ -0,0 +1,74 @@
+// controllers/usersController.js
+const pool = require('../db/db');
+
+const getUsers = async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao obter usuários');
+  }
+};
+
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+    if (result.rows.length === 0) {
+      return res.status(404).send('Usuário não encontrado');
+    }
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao obter usuário');
+  }
+};
+
+const createUser = async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(
+      'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
+      [name, email]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao criar usuário');
+  }
+};
+
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(
+      'UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *',
+      [name, email, id]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).send('Usuário não encontrado');
+    }
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao atualizar usuário');
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query('DELETE FROM users WHERE id = $1 RETURNING *', [id]);
+    if (result.rows.length === 0) {
+      return res.status(404).send('Usuário não encontrado');
+    }
+    res.status(204).send();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao excluir usuário');
+  }
+};
+
+module.exports = { getUsers, getUserById, createUser, updateUser, deleteUser };
